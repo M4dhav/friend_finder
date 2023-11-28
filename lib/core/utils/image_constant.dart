8e4a2b5edf7cb92ef02e10d118155f21f0e7fc98
Ace0Blade <144112474+Ace0Blade@users.noreply.github.com>
2023-11-28 11:17:15 +0530

    Add files via upload

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,68 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Login Page images
+  static String imgRectangle = '$imagePath/img_rectangle.png';
+
+  static String imgRectangle76x76 = '$imagePath/img_rectangle_76x76.png';
+
+  static String imgRectangle1 = '$imagePath/img_rectangle_1.png';
+
+  static String imgRectangle2 = '$imagePath/img_rectangle_2.png';
+
+  static String imgRectangle3 = '$imagePath/img_rectangle_3.png';
+
+  static String imgRectangle4 = '$imagePath/img_rectangle_4.png';
+
+  static String imgRectangle5 = '$imagePath/img_rectangle_5.png';
+
+  static String imgRectangle6 = '$imagePath/img_rectangle_6.png';
+
+  static String imgRectangle7 = '$imagePath/img_rectangle_7.png';
+
+  static String imgRectangle8 = '$imagePath/img_rectangle_8.png';
+
+  static String imgRectangle9 = '$imagePath/img_rectangle_9.png';
+
+  static String imgRectangle10 = '$imagePath/img_rectangle_10.png';
+
+  static String imgRectangle11 = '$imagePath/img_rectangle_11.png';
+
+  static String imgRectangle12 = '$imagePath/img_rectangle_12.png';
+
+  static String imgRectangle13 = '$imagePath/img_rectangle_13.png';
+
+  static String imgRectangle14 = '$imagePath/img_rectangle_14.png';
+
+  static String imgRectangle15 = '$imagePath/img_rectangle_15.png';
+
+  static String imgRectangle16 = '$imagePath/img_rectangle_16.png';
+
+  static String imgRectangle17 = '$imagePath/img_rectangle_17.png';
+
+  static String imgRectangle18 = '$imagePath/img_rectangle_18.png';
+
+  static String imgUntitledDesign = '$imagePath/img_untitled_design.png';
+
+  static String imgMicrosoftOutlook = '$imagePath/img_microsoft_outlook.svg';
+
+  // Common images
+  static String imgGroup6 = '$imagePath/img_group_6.png';
+
+  static String imgPeople = '$imagePath/img_people.png';
+
+  static String imgUserAccount = '$imagePath/img_user_account.png';
+
+  static String imgGroup13 = '$imagePath/img_group_13.svg';
+
+  static String imgGroup15 = '$imagePath/img_group_15.svg';
+
+  static String imgNavMaps = '$imagePath/img_nav_maps.svg';
+
+  static String imgNavFriends = '$imagePath/img_nav_friends.svg';
+
+  static String imgNavAccount = '$imagePath/img_nav_account.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
