bfc31ee437c85c150da6fb842423c3af66940c11
Ace0Blade <144112474+Ace0Blade@users.noreply.github.com>
2023-11-28 11:17:46 +0530

    Add files via upload

---
+++ lib/widgets/custom_bottom_bar.dart
@@ -0,0 +1,169 @@
+import 'package:abhishek_s_application1/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomBottomBar extends StatefulWidget {
+  CustomBottomBar({this.onChanged});
+
+  Function(BottomBarEnum)? onChanged;
+
+  @override
+  CustomBottomBarState createState() => CustomBottomBarState();
+}
+
+class CustomBottomBarState extends State<CustomBottomBar> {
+  int selectedIndex = 0;
+
+  List<BottomMenuModel> bottomMenuList = [
+    BottomMenuModel(
+      icon: ImageConstant.imgNavMaps,
+      activeIcon: ImageConstant.imgNavMaps,
+      title: "Maps",
+      type: BottomBarEnum.Maps,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgNavFriends,
+      activeIcon: ImageConstant.imgNavFriends,
+      title: "Friends",
+      type: BottomBarEnum.Friends,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgNavAccount,
+      activeIcon: ImageConstant.imgNavAccount,
+      title: "Account",
+      type: BottomBarEnum.Account,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 81.v,
+      decoration: BoxDecoration(
+        color: theme.colorScheme.primary,
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30.h),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: appTheme.black900.withOpacity(0.25),
+            spreadRadius: 2.h,
+            blurRadius: 2.h,
+            offset: Offset(
+              0,
+              4,
+            ),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedFontSize: 0,
+        elevation: 0,
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: List.generate(bottomMenuList.length, (index) {
+          return BottomNavigationBarItem(
+            icon: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomImageView(
+                  imagePath: bottomMenuList[index].icon,
+                  height: 23.v,
+                  width: 30.h,
+                  color: appTheme.black900,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 4.v),
+                  child: Text(
+                    bottomMenuList[index].title ?? "",
+                    style: theme.textTheme.labelMedium!.copyWith(
+                      color: appTheme.black900,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            activeIcon: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomImageView(
+                  imagePath: bottomMenuList[index].activeIcon,
+                  height: 23.v,
+                  width: 18.h,
+                  color: appTheme.black900,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 6.v),
+                  child: Text(
+                    bottomMenuList[index].title ?? "",
+                    style: theme.textTheme.labelMedium!.copyWith(
+                      color: appTheme.black900,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            label: '',
+          );
+        }),
+        onTap: (index) {
+          selectedIndex = index;
+          widget.onChanged?.call(bottomMenuList[index].type);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
+
+enum BottomBarEnum {
+  Maps,
+  Friends,
+  Account,
+}
+
+class BottomMenuModel {
+  BottomMenuModel({
+    required this.icon,
+    required this.activeIcon,
+    this.title,
+    required this.type,
+  });
+
+  String icon;
+
+  String activeIcon;
+
+  String? title;
+
+  BottomBarEnum type;
+}
+
+class DefaultWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Please replace the respective Widget here',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
